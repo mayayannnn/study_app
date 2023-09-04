3a3d6745ab8b7d897b8e1318d4ba6f80f3468eb3
mayayannnn <sakamoto-t.m@nifty.com>
2023-09-04 19:20:23 +0900

    add_question

--- app/controllers/board_controller.rb
+++ app/controllers/board_controller.rb
@@ -6,5 +6,33 @@ class BoardController < ApplicationController
   def ask_quesiton
   end
   def content
+    @borads = Board.where(subject: params[:id])
+    @title = ""
+    if params[:id] == "math"
+      @title = "数学"
+    elsif params[:id] == "japanese"
+      @title = "国語"
+    elsif params[:id] == "science"
+      @title = "理科"
+    elsif params[:id] == "society"
+      @title = "社会"
+    elsif params[:id] == "english"
+      @title = "英語"
+    elsif params[:id] == "it"
+      @title = "情報"
+    end
+  end
+  def create
+    @content = Board.new(content:params[:content],
+                         subject:params[:subject])
+    @content.save
+    redirect_to("/")
+  end
+  def create_question
+    @content = Question.new(content:params[:content],
+                            subject:params[:subject],
+                            answer:params[:answer])
+    @content.save
+    redirect_to("/")
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   get 'ai_question/home'
+
   get '/' => "board#home"
   get "/add_ask_question" => "board#add_ask_question"
   get "/add_set_question" => "board#add_set_question"
   get "/content/:id" => "board#content"
+  post "/create" => "board#create"
+  post "/create_question" => "board#create_question"
+  get "/content/:id/question" => "board#content_question"
+
   get "/ai_question" => "ai_question#home"
   get "/question_for_ai" => "ai_question#question_for_ai"
+  
 end
